7521ead2861022440abd3a4c792d7bcf5ea5e350
Doug Waldron <737326+dougwaldron@users.noreply.github.com>
2025-03-24 14:22:12 -0400

    Add XML docs to new methods and prefer URL over URI in naming (#52)

--- src/AppLibrary/Apis/ApiUtilities.cs
+++ src/AppLibrary/Apis/ApiUtilities.cs
@@ -5,26 +5,50 @@ namespace GaEpd.AppLibrary.Apis;
 
 public static class ApiUtilities
 {
+    /// <summary>
+    /// Fetches JSON data from an API endpoint and deserializes it to the target type.
+    /// This method does not handle authentication.
+    /// </summary>
+    /// <param name="httpClientFactory">The <see cref="T:System.Net.Http.IHttpClientFactory" /> available in your app.</param>
+    /// <param name="apiUrl">The base URL for the API to connect to.</param>
+    /// <param name="endpointPath">The relative path for the API endpoint to connect to.</param>
+    /// <param name="clientName">A logical name for the <see cref="T:System.Net.Http.HttpClient" />.</param>
+    /// <typeparam name="T">The target type to deserialize the API data to.</typeparam>
+    /// <returns>API data deserialized to the target type.</returns>
     public static async Task<T?> FetchApiDataAsync<T>(this IHttpClientFactory httpClientFactory,
         Uri apiUrl, string endpointPath, string clientName = "")
     {
         using var client = httpClientFactory.CreateClient(clientName);
-        using var response = await client.GetAsync(UriCombine(apiUrl, endpointPath)).ConfigureAwait(false);
+        using var response = await client.GetAsync(UrlCombine(apiUrl, endpointPath)).ConfigureAwait(false);
         response.EnsureSuccessStatusCode();
         return await response.Content.ReadFromJsonAsync<T>().ConfigureAwait(false);
     }
 
-    public static Uri UriCombine(string baseUri, string? relativeUri) =>
-        UriCombine(new Uri(Guard.NotNullOrWhiteSpace(baseUri)), relativeUri);
+    /// <summary>
+    /// Combine a base URL and a relative URL path, correctly handling path separators.
+    /// The base URL must be an absolute URL.
+    /// </summary>
+    /// <param name="baseUrl">The base URL.</param>
+    /// <param name="relativeUrl">The relative path.</param>
+    /// <returns>The combined URL.</returns>
+    public static Uri UrlCombine(string baseUrl, string? relativeUrl) =>
+        UrlCombine(new Uri(Guard.NotNullOrWhiteSpace(baseUrl)), relativeUrl);
 
-    public static Uri UriCombine(Uri baseUri, string? relativeUri)
+    /// <summary>
+    /// Combine a base URL and a relative URL path, correctly handling path separators.
+    /// The base URL must be an absolute URL.
+    /// </summary>
+    /// <param name="baseUrl">The base URL.</param>
+    /// <param name="relativeUrl">The relative path.</param>
+    /// <returns>The combined URL.</returns>
+    public static Uri UrlCombine(Uri baseUrl, string? relativeUrl)
     {
-        Guard.NotNull(baseUri);
-        if (!baseUri.IsAbsoluteUri) throw new ArgumentOutOfRangeException(nameof(baseUri));
+        Guard.NotNull(baseUrl);
+        if (!baseUrl.IsAbsoluteUri) throw new ArgumentOutOfRangeException(nameof(baseUrl));
 
-        if (string.IsNullOrEmpty(relativeUri)) return baseUri;
+        if (string.IsNullOrEmpty(relativeUrl)) return baseUrl;
 
         const char separator = '/';
-        return new Uri(baseUri.ToString().TrimEnd(separator) + separator + relativeUri.TrimStart(separator));
+        return new Uri(baseUrl.ToString().TrimEnd(separator) + separator + relativeUrl.TrimStart(separator));
     }
 }
