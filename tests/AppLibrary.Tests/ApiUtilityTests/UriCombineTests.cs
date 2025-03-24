30071b1e8c6f74ae4fa53e127711288a99909f08
Doug Waldron <douglas.waldron@dnr.ga.gov>
2025-03-24 13:56:19 -0400

    Add XML docs to new methods and prefer URL over URI in naming

--- tests/AppLibrary.Tests/ApiUtilityTests/UriCombineTests.cs
+++ tests/AppLibrary.Tests/ApiUtilityTests/UriCombineTests.cs
@@ -1,8 +1,8 @@
 ﻿using GaEpd.AppLibrary.Apis;
 
-namespace AppLibrary.Tests.ExtensionTests;
+namespace AppLibrary.Tests.ApiUtilityTests;
 
-public class UriCombineTests
+public class UrlCombineTests
 {
     private const string BaseUri = "https://localhost";
     private const string BaseUriWithTrailingSlash = "https://localhost/";
@@ -16,9 +16,9 @@ public class UriCombineTests
     [TestCase(BaseUriWithTrailingSlash, Path)]
     [TestCase(BaseUri, PathWithLeadingSlash)]
     [TestCase(BaseUriWithTrailingSlash, PathWithLeadingSlash)]
-    public void UriCombine_WithValidSegments_ReturnsCombinedUri(string baseUri, string? relativeUri)
+    public void UrlCombine_WithValidSegments_ReturnsCombinedUri(string baseUri, string? relativeUri)
     {
-        ApiUtilities.UriCombine(new Uri(baseUri), relativeUri)
+        ApiUtilities.UrlCombine(new Uri(baseUri), relativeUri)
             .Should().Be(new Uri(ExpectedResultWithPath));
     }
 
@@ -26,62 +26,62 @@ public class UriCombineTests
     [TestCase(BaseUriWithTrailingSlash, Path)]
     [TestCase(BaseUri, PathWithLeadingSlash)]
     [TestCase(BaseUriWithTrailingSlash, PathWithLeadingSlash)]
-    public void UriCombine_WithValidSegments_AsStrings_ReturnsCombinedUri(string baseUri, string? relativeUri)
+    public void UrlCombine_WithValidSegments_AsStrings_ReturnsCombinedUri(string baseUri, string? relativeUri)
     {
-        ApiUtilities.UriCombine(baseUri, relativeUri)
+        ApiUtilities.UrlCombine(baseUri, relativeUri)
             .Should().Be(new Uri(ExpectedResultWithPath));
     }
 
     [TestCase(BaseUri)]
     [TestCase(BaseUriWithTrailingSlash)]
-    public void UriCombine_WithNullPath_ReturnsBaseUri(string baseUri)
+    public void UrlCombine_WithNullPath_ReturnsBaseUri(string baseUri)
     {
-        ApiUtilities.UriCombine(new Uri(baseUri), null)
+        ApiUtilities.UrlCombine(new Uri(baseUri), null)
             .Should().Be(new Uri(ExpectedResultWithoutPath));
     }
 
     [TestCase(BaseUri)]
     [TestCase(BaseUriWithTrailingSlash)]
-    public void UriCombine_WithStringBase_AndNullPath_ReturnsBaseUri(string baseUri)
+    public void UrlCombine_WithStringBase_AndNullPath_ReturnsBaseUri(string baseUri)
     {
-        ApiUtilities.UriCombine(baseUri, null)
+        ApiUtilities.UrlCombine(baseUri, null)
             .Should().Be(new Uri(ExpectedResultWithoutPath));
     }
 
     [TestCase(BaseUri)]
     [TestCase(BaseUriWithTrailingSlash)]
-    public void UriCombine_WithEmptyPath_ReturnsBaseUri(string baseUri)
+    public void UrlCombine_WithEmptyPath_ReturnsBaseUri(string baseUri)
     {
-        ApiUtilities.UriCombine(new Uri(baseUri), "")
+        ApiUtilities.UrlCombine(new Uri(baseUri), "")
             .Should().Be(new Uri(ExpectedResultWithoutPath));
     }
 
     [TestCase(BaseUri)]
     [TestCase(BaseUriWithTrailingSlash)]
-    public void UriCombine_WithStringBase_AndEmptyPath_ReturnsBaseUri(string baseUri)
+    public void UrlCombine_WithStringBase_AndEmptyPath_ReturnsBaseUri(string baseUri)
     {
-        ApiUtilities.UriCombine(baseUri, "")
+        ApiUtilities.UrlCombine(baseUri, "")
             .Should().Be(new Uri(ExpectedResultWithoutPath));
     }
 
     [Test]
-    public void UriCombine_WithEmptyBase_Throws()
+    public void UrlCombine_WithEmptyBase_Throws()
     {
-        var func = () => ApiUtilities.UriCombine("", null);
+        var func = () => ApiUtilities.UrlCombine("", null);
         func.Should().Throw<ArgumentException>();
     }
 
     [Test]
-    public void UriCombine_WithInvalidBase_Throws()
+    public void UrlCombine_WithInvalidBase_Throws()
     {
-        var func = () => ApiUtilities.UriCombine("invalid", null);
+        var func = () => ApiUtilities.UrlCombine("invalid", null);
         func.Should().Throw<UriFormatException>();
     }
 
     [Test]
-    public void UriCombine_WithRelativeBase_Throws()
+    public void UrlCombine_WithRelativeBase_Throws()
     {
-        var func = () => ApiUtilities.UriCombine(new Uri("localhost", UriKind.Relative), null);
+        var func = () => ApiUtilities.UrlCombine(new Uri("localhost", UriKind.Relative), null);
         func.Should().Throw<ArgumentOutOfRangeException>();
     }
 }
