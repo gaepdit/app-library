7c1ed4b92a409a04a16da1e36005fee2bfb40bab
Doug Waldron <douglas.waldron@dnr.ga.gov>
2025-07-15 14:54:30 -0400

    Prevent empty sorting value in Paginated Request

--- tests/AppLibrary.Tests/PaginationTests/PaginatedRequestTests.cs
+++ tests/AppLibrary.Tests/PaginationTests/PaginatedRequestTests.cs
@@ -7,12 +7,15 @@ public class PaginatedRequestTests
     [Test]
     public void ReturnsWithPagingGivenValidInputs()
     {
+        // Arrange
         const int pageNumber = 2;
         const int pageSize = 10;
         const string sorting = "Note";
 
+        // Act
         var pagination = new PaginatedRequest(pageNumber, pageSize, sorting);
 
+        // Assert
         using var scope = new AssertionScope();
         pagination.PageSize.Should().Be(pageSize);
         pagination.PageNumber.Should().Be(pageNumber);
@@ -24,22 +27,58 @@ public class PaginatedRequestTests
     [Test]
     public void ThrowsExceptionGivenZeroPageSize()
     {
+        // Arrange
         const int pageNumber = 2;
         const int pageSize = 0;
 
+        // Act
         var action = () => new PaginatedRequest(pageNumber, pageSize, "_");
-        action.Should().Throw<ArgumentException>()
-            .And.ParamName.Should().Be(nameof(pageSize));
+
+        // Assert
+        action.Should().Throw<ArgumentException>().And.ParamName.Should().Be(nameof(pageSize));
     }
 
     [Test]
     public void ThrowsExceptionGivenZeroPageNumber()
     {
+        // Arrange
         const int pageNumber = 0;
         const int pageSize = 20;
 
+        // Act
         var action = () => new PaginatedRequest(pageNumber, pageSize, "_");
-        action.Should().Throw<ArgumentException>()
-            .And.ParamName.Should().Be(nameof(pageNumber));
+
+        // Assert
+        action.Should().Throw<ArgumentException>().And.ParamName.Should().Be(nameof(pageNumber));
+    }
+
+    [Test]
+    public void ThrowsExceptionGivenNullSorting()
+    {
+        // Arrange
+        const int pageNumber = 1;
+        const int pageSize = 20;
+
+        // Act
+#pragma warning disable CS8625 // Cannot convert null literal to non-nullable reference type.
+        var action = () => new PaginatedRequest(pageNumber, pageSize, null);
+#pragma warning restore CS8625 // Cannot convert null literal to non-nullable reference type.
+
+        // Assert
+        action.Should().Throw<ArgumentException>().And.ParamName.Should().Be("sorting");
+    }
+
+    [Test]
+    public void ThrowsExceptionGivenEmptySorting()
+    {
+        // Arrange
+        const int pageNumber = 1;
+        const int pageSize = 20;
+
+        // Act
+        var action = () => new PaginatedRequest(pageNumber, pageSize, string.Empty);
+
+        // Assert
+        action.Should().Throw<ArgumentException>().And.ParamName.Should().Be("sorting");
     }
 }
